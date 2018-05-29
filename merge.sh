2ffff553dcf5cb3bb95b88856bda6d39d8573b7f
Allan Ebdrup <allan@878.dk>
2018-05-29 13:46:46 +0200

    fix for Oliver

--- merge.sh
+++ merge.sh
@@ -258,6 +258,7 @@ pullRequestLink="<https://github.com/practio/${project}/pull/${pullRequestNumber
 step_start "Checking out master, resetting (hard), pulling from origin and cleaning"
 
 git checkout master || build_done $? "Could not checkout master"
+git branch --set-upstream-to=origin/master master || build_done $? "Could not set upstream for master to origin/master"
 git reset --hard origin/master || build_done $? "Could not reset to master"
 git pull || build_done $? "Could not pull master"
 git clean -fx || build_done $? "Could not git clean on master"
