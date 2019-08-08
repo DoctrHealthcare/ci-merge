31e4fbf8ccc9e421653ce6176c4e7e09a3dc752c
Teamcity <build@practio.com>
2019-08-08 16:23:34 +0100

    Fixes e2e handling

--- pull-request.sh
+++ pull-request.sh
@@ -309,13 +309,20 @@ exec 5>&1
 ## redirect stderr to stdout for capture by tee, and redirect stdout to file descriptor 5 for output on stdout (with no capture by tee)
 ## after capture of stderr on stdout by tee, redirect back to stderr
 npm run teamcity 2>&1 1>&5 | tee err.log 1>&2
+exit_code=${PIPESTATUS[0]}
 
+## Executes e2e tests
+## In the end the server process gets killed
+## which causes this main process to die too
+## To avoid that, we run e2e tests in a different process
+## and wait for it to finish
 teamcity_e2e_script=$(node -e "console.log(require('./package.json').scripts['teamcity:e2e'] || '')")
-if [ "$teamcity_e2e_script" != '' ]
+if [ "$exit_code" == "0" ] && [ "$teamcity_e2e_script" != '' ]
 then
   npm run teamcity:e2e &
   proc=$!
   wait $proc
+  $exit_code=$?
 fi
 
-after_teamcity_script ${PIPESTATUS[0]}
+after_teamcity_script $exit_code
