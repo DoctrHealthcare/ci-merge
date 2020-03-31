b56011c6b17af5457f19504728eae549e2b19891
Allan Ebdrup <allan@878.dk>
2020-03-31 16:29:44 +0200

    add retries to npm commands

--- merge.sh
+++ merge.sh
@@ -72,8 +72,8 @@ package_update(){
 	*_package_update)
 		add_npm_token || _exit $? "Adding NPM_TOKEN env var to .npmrc failed"
 		echo "Doing npm install because we are running a package update ready branch"
-		npm install || _exit $? "npm install failed (_package_update branch name)"
-		npm audit fix --package-lock-only || _exit $? "npm audit fix failed (_package_update branch name)"
+		(retry 2 npm install) || _exit $? "npm install failed (_package_update branch name)"
+		(retry 4 npm audit fix --package-lock-only) || _exit $? "npm audit fix failed (_package_update branch name)"
 		(git add package-lock.json && git commit -m "update package-lock.json" --author "${lastCommitAuthor}") || echo "Ignoring: Could not git add package-lock.json, maybe no changes"
 		remove_npm_token || _exit $? "Removing NPM_TOKEN env var from .npmrc failed"
 		;;
